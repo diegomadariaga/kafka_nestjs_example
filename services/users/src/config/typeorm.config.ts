7f92451e114d8e8e2d72e039c0f61c4105d78261
diegomadariaga <diegonicolasmadariaga@gmail.com>
2025-07-04 00:11:32 -0400

    feat: update TypeORM configuration for SQLite database path and add .gitignore for database files

--- services/users/src/config/typeorm.config.ts
+++ services/users/src/config/typeorm.config.ts
@@ -1,9 +1,10 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { join } from 'path';
 import { User } from '../entities/user.entity';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'sqlite',
-  database: 'users.db',
+  database: join(__dirname, '../../../../../users.db'), // Base de datos en la raíz del proyecto
   entities: [User],
   synchronize: process.env.NODE_ENV !== 'prod', // Solo en desarrollo
   logging: process.env.NODE_ENV !== 'prod',
